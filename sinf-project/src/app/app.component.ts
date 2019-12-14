b679221571e641816b540e5193d629a2fc4060ee
MiguelFerreira1998 <33258304+MiguelFerreira1998@users.noreply.github.com>
2019-12-14 20:38:39 +0000

    parse do financial saft completo

--- sinf-project/src/app/app.component.ts
+++ sinf-project/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 //Testing
 import {Customer, Product, Venda, Invoice } from './model/client_model';
-import {Account, Customer2} from './model/financial_model';
+import {Account, Customer2, Address, Supplier, Tax, Ledger, CreditLine, DebitLine, Journal, Transaction} from './model/financial_model';
 import { find } from 'rxjs/operators';
 
 
@@ -67,12 +67,15 @@ export class AppComponent {
       parser.parseString(data, function (err, result) {  
 
         var accounts_array = new Array<Account>();
+        var customers_array = new Array<Customer2>();
+        var supplier_array = new Array<Supplier>();
+        var taxes_array = new Array<Tax>();
+        var ledger = new Ledger();
 
         var obj = result.AuditFile;  
         var accountsxml = obj.MasterFiles[0].GeneralLedgerAccounts[0].Account;
-        console.log(accountsxml);
-        //PARSE ACCOUNTS
 
+        //PARSE ACCOUNTS
         accountsxml.forEach(element => {
           let temp = new Account();
           temp.accountID = element.AccountID;
@@ -87,28 +90,153 @@ export class AppComponent {
         console.log(accounts_array);
 
         //Parse Customers
-        var customersxml = obj.MasterFiles[0].Custumer;
+        var customersxml = obj.MasterFiles[0].Customer;
+        customersxml.forEach(element => {
+            let tempcustomer = new Customer2();
+            tempcustomer.customerID = element.CustomerID[0];
+            tempcustomer.accountID = element.AccountID[0];
+            tempcustomer.customerTaxID = element.CustomerTaxID[0];
+            tempcustomer.companyName = element.CompanyName[0];
+          
+          let tempbillingaddress = new Address();
+            tempbillingaddress.addressDetail = element.BillingAddress[0].AddressDetail[0];
+            tempbillingaddress.city = element.BillingAddress[0].City[0];
+            tempbillingaddress.postalCode = element.BillingAddress[0].PostalCode[0];
+            tempbillingaddress.region = element.BillingAddress[0].Region[0];
+            tempbillingaddress.country = element.BillingAddress[0].Country[0];
+          tempcustomer.billingAddress = tempbillingaddress;
 
-        /*
-        export class Customer{
-          customerID: number;
-          accountID: string;
-          customerTaxID: number;
-          companyName: string;
-          ClosingDebitBalance: number;
-          billingAddress: Address;
-          shipToAddress: Address
-          telephone: number;
-          selfBillingIndicator: number;
-      }
-      export class Address{
-          addressDetail: string;
-          city: string;
-          postalCode: string;
-          region: string;
-          country: string;
-      }*/
+          let tempshipaddress = new Address();
+            tempshipaddress.addressDetail = element.BillingAddress[0].AddressDetail[0];
+            tempshipaddress.city = element.BillingAddress[0].City[0];
+            tempshipaddress.postalCode = element.BillingAddress[0].PostalCode[0];
+            tempshipaddress.region = element.BillingAddress[0].Region[0];
+            tempshipaddress.country = element.BillingAddress[0].Country[0];
+          tempcustomer.shipToAddress = tempshipaddress;
+          tempcustomer.selfBillingIndicator = element.SelfBillingIndicator[0];
+          customers_array.push(tempcustomer);
+        });
+        console.log(customers_array);
 
+        //PARSE SUPPLIERS
+        var suppliersxml= obj.MasterFiles[0].Supplier;
+        console.log(suppliersxml);
+        suppliersxml.forEach(element => {
+          let suppliertemp = new Supplier();
+            suppliertemp.supplierID = element.SupplierID[0];
+            suppliertemp.accountID = element.AccountID[0];
+            suppliertemp.supplierTaxID = element.SupplierTaxID[0];
+            suppliertemp.companyName = element.CompanyName[0];
+
+            let tempbillingaddress = new Address();
+
+            tempbillingaddress.addressDetail = element.BillingAddress[0].AddressDetail[0];
+            tempbillingaddress.city = element.BillingAddress[0].City[0];
+            tempbillingaddress.postalCode = element.BillingAddress[0].PostalCode[0];
+            tempbillingaddress.region = element.BillingAddress[0].Region[0];
+            tempbillingaddress.country = element.BillingAddress[0].Country[0];
+            suppliertemp.billingAddress = tempbillingaddress;
+
+            let tempshipaddress = new Address();
+            tempshipaddress.addressDetail = element.BillingAddress[0].AddressDetail[0];
+            tempshipaddress.city = element.BillingAddress[0].City[0];
+            tempshipaddress.postalCode = element.BillingAddress[0].PostalCode[0];
+            tempshipaddress.region = element.BillingAddress[0].Region[0];
+            tempshipaddress.country = element.BillingAddress[0].Country[0];
+            suppliertemp.shipToAddress = tempshipaddress;
+
+            suppliertemp.selfBillingIndicator = element.SelfBillingIndicator[0];
+            
+            supplier_array.push(suppliertemp);
+        });
+        console.log(supplier_array);
+
+        //Parse TAXES
+        
+        var taxesxml= obj.MasterFiles[0].TaxTable[0].TaxTableEntry;
+
+        taxesxml.forEach(element => {
+          let temptax = new Tax();
+          temptax.taxType = element.TaxType[0];
+          temptax.taxCountryRegion = element.TaxCountryRegion[0];
+          temptax.taxCode = element.TaxCode[0];
+          temptax.description = element.Description[0];
+          temptax.taxPercentage = element.TaxPercentage[0];
+          taxes_array.push(temptax);
+        });
+        console.log(taxes_array);
+        
+        //PARSE GeneralLedgerEntries
+        let ledgerxml = obj.GeneralLedgerEntries[0];
+        ledger.numberOfEntries = ledgerxml.NumberOfEntries[0];
+        ledger.totalDebit = ledgerxml.TotalDebit[0];
+        ledger.totalCredit = ledgerxml.TotalCredit[0];
+        
+        let journalxml = obj.GeneralLedgerEntries[0].Journal;
+        
+        var journals_array = new Array<Journal>();
+        //EACH JOURNAL      
+        journalxml.forEach(element => {
+          let journaltemp = new Journal();
+            journaltemp.journalID = element.JournalID[0];
+            journaltemp.description = element.Description[0];
+
+            //PARSE TRANSACTIONS
+            let transactionxml = element.Transaction;
+            var transaction_arraytemp = new Array<Transaction>();
+
+            transactionxml.forEach(element => {
+                let transaction_temp = new Transaction();
+                transaction_temp.transactionID = element.TransactionID[0];
+                transaction_temp.period = element.Period[0];
+                transaction_temp.transactionDate = element.TransactionDate[0];
+                transaction_temp.sourceID = element.SourceID[0];
+                transaction_temp.description = element.Description[0];
+                transaction_temp.docArchivalNumber = element.DocArchivalNumber[0];
+                transaction_temp.transactionType = element.TransactionType[0];
+                transaction_temp.gLPostingDate = element.GLPostingDate[0];
+
+                //PARSE DEBIT/CREDIT LINES
+              
+                var debitlines_array = new Array<DebitLine>();
+                var creditlines_array = new Array<CreditLine>();
+              
+                //DEBIT LINE
+                let transaction_debitlinesxml = element.Lines[0].DebitLine;
+                    transaction_debitlinesxml.forEach(element => {
+                        let debitLinetemp = new DebitLine();
+                        debitLinetemp.recordID = element.RecordID[0];
+                        debitLinetemp.accountID = element.AccountID[0];
+                        //debitLinetemp.sourceDocumentID = element.SourceDocumentID[0];
+                        debitLinetemp.systemEntryDate = element.SystemEntryDate[0];
+                        debitLinetemp.description = element.Description[0];
+                        debitLinetemp.debitAmount = element.DebitAmount[0];
+                        debitlines_array.push(debitLinetemp);
+                    });
+                transaction_temp.debitLines = debitlines_array;
+
+                //CREDIT LINE
+                let transaction_creditlinesxml = element.Lines[0].CreditLine;
+
+                transaction_creditlinesxml.forEach(element => {
+                        let creditLinetemp = new CreditLine();
+                        creditLinetemp.recordID = element.RecordID[0];
+                        creditLinetemp.accountID = element.AccountID[0];
+                        //creditLinetemp.sourceDocumentID = element.SourceDocumentID[0];
+                        creditLinetemp.systemEntryDate = element.SystemEntryDate[0];
+                        creditLinetemp.description = element.Description[0];
+                        creditLinetemp.creditAmount = element.CreditAmount[0];
+                        creditlines_array.push(creditLinetemp);
+                  });
+                transaction_temp.creditLines = creditlines_array;
+                transaction_arraytemp.push(transaction_temp);
+            });
+                //ADD TO OTHER TRANSACTIONS
+                journaltemp.transactions = transaction_arraytemp;
+                journals_array.push(journaltemp);
+        });
+          ledger.journals = journals_array;
+          console.log(ledger);
       })
     });
   }
--- sinf-project/src/app/model/financial_model.ts
+++ sinf-project/src/app/model/financial_model.ts
@@ -9,18 +9,16 @@ export class Account{
     groupingCategory: string;
 }
 
-
 export class Customer2{
     customerID: number;
     accountID: string;
     customerTaxID: number;
     companyName: string;
-    ClosingDebitBalance: number;
     billingAddress: Address;
     shipToAddress: Address
-    telephone: number;
     selfBillingIndicator: number;
-}
+}  
+
 export class Address{
     addressDetail: string;
     city: string;
@@ -28,3 +26,66 @@ export class Address{
     region: string;
     country: string;
 }
+
+export class Supplier{
+    supplierID: number;
+    accountID: string;
+    supplierTaxID: number;
+    companyName: string;
+    billingAddress: Address;
+    shipToAddress: Address
+    selfBillingIndicator: number;
+}
+
+export class Tax{
+    taxType: string;
+    taxCountryRegion: string;
+    taxCode: string;
+    description: string;
+    taxPercentage: number;
+}
+
+export class Ledger{ //so existe 1
+    numberOfEntries: number;
+    totalDebit: number;
+    totalCredit: number;
+    journals: Array<Journal> = [];
+}
+
+export class Journal{ 
+    journalID: number;
+    description: string;
+    transactions: Array<Transaction> = [];
+}
+
+export class Transaction{ 
+    transactionID: string;
+    period: number;
+    transactionDate: string;
+    sourceID: string;
+    description: string;
+    docArchivalNumber: number;
+    transactionType: string;
+    gLPostingDate: string;
+    debitLines: Array<DebitLine> = [];
+    creditLines: Array<CreditLine> = [];
+}
+
+export class DebitLine{ 
+    recordID: string;
+    accountID: number;
+    sourceDocumentID: string;
+    systemEntryDate: string;
+    description: string;
+    debitAmount: number;
+}
+
+export class CreditLine{ 
+    recordID: string;
+    accountID: number;
+    sourceDocumentID: string;
+    systemEntryDate: string;
+    description: string;
+    creditAmount: number;
+}
+
